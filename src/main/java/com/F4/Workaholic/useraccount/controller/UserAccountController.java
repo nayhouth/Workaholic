d318aece74bfc3c80cd42e84ec72a95abdfc47f7
nayhouth <yem.nayhout@gmail.com>
2026-09-04 21:21:45 +0700

    refactor: use generic core backend controller

--- src/main/java/com/F4/Workaholic/useraccount/controller/UserAccountController.java
+++ src/main/java/com/F4/Workaholic/useraccount/controller/UserAccountController.java
@@ -2,45 +2,16 @@ package com.F4.Workaholic.useraccount.controller;
 
 import com.F4.Workaholic.useraccount.entity.UserAccount;
 import com.F4.Workaholic.useraccount.service.UserAccountService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.corebackend.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user-accounts")
-public class UserAccountController {
-
-    private final UserAccountService service;
+public class UserAccountController
+        extends BaseController<UserAccount> {
 
     public UserAccountController(UserAccountService service) {
-        this.service = service;
-    }
-
-    @PostMapping
-    public UserAccount create(@RequestBody UserAccount userAccount) {
-        return service.create(userAccount);
-    }
-
-    @GetMapping
-    public List<UserAccount> getAll() {
-        return service.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public UserAccount getById(@PathVariable Long id) {
-        return service.getById(id).orElse(null);
-    }
-
-    @PutMapping("/{id}")
-    public UserAccount update(
-            @PathVariable Long id,
-            @RequestBody UserAccount userAccount) {
-
-        return service.update(id, userAccount);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        service.delete(id);
+        super(service);
     }
 }
